eea1809f8e3ddd3faf247b9b2df8fd05a36a868c
peiyuan <87107748+letsgopy@users.noreply.github.com>
2021-12-04 23:36:36 +0800

    pull commit

--- src/test.java
+++ src/test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }

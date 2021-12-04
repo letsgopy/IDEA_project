937641698aaa4709015c40ca5254c758699a3549
lipeiyuan <xiaoli1536@163.com>
2021-12-04 22:53:02 +0800

    hot-fix commit2

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class test {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
     }
 }
